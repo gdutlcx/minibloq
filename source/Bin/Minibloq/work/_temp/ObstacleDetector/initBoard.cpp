e49352a07ac656d7bfb48e114f11b8ec6959333f
rahulratheesh <rahul_r03@yahoo.in>
2014-06-20 07:18:05 +0530

    New examples

---
+++ source/Bin/Minibloq/work/_temp/ObstacleDetector/initBoard.cpp
@@ -0,0 +1,17 @@
+#include <mbq.h>
+
+
+Adafruit_SSD1306 display(9, 10, 11, 13, 12);
+
+
+void initBoard()
+{
+	Serial.begin(115200);
+
+	//Uses the analog input 0, that may have the same value in some designs, but it's not so prabably:
+	randomSeed(analogRead(0));
+
+	display.begin(SSD1306_SWITCHCAPVCC);
+	display.display();
+	delay(1000);
+}
---
+++ source/Bin/Minibloq/work/_temp/ObstacleDetector/output/Adafruit_SSD1306.cpp.d
@@ -0,0 +1,19 @@
+ \
+ D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq\work\_temp/ObstacleDetector/output/Adafruit_SSD1306.cpp.o:  \
+ D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/libs/Arduino/Adafruit_SSD1306/Adafruit_SSD1306.cpp \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/libs/Arduino/Wire/Wire.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Stream.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Print.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/WString.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Printable.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/new.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/libs/Arduino/Adafruit_GFX/Adafruit_GFX.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Arduino.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/binary.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/WCharacter.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/HardwareSerial.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Stream.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/variants/standard/pins_arduino.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/cores/Arduino.v1.5.4.r2/avr/Print.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/libs/Arduino/Adafruit_SSD1306/Adafruit_SSD1306.h \
+  D:\MySEProject\ERayRobot\v0.82-master\v0.82-master\source\Bin\Minibloq/libs/Arduino/SPI/SPI.h
